b1a810f8e00c7b36c2fa9aa4338dc36c60aa6d0b
KXRXH <kirayfolon@gmail.com>
2021-12-11 23:26:35 +0700

    price change added

--- backend/api/api.go
+++ backend/api/api.go
@@ -79,6 +79,10 @@ func (api *ApiInterface) InitApp() {
 	api.App.Post("/api/price/", controllers.CreatePrice)
 	api.App.Put("/api/price/:id", controllers.UpdatePrice)
 	api.App.Delete("/api/price/:id", controllers.DeletePrice)
+	// PriceChange
+	api.App.Get("/api/pricechange/", controllers.GetAllPriceChange)
+	api.App.Post("/api/pricechange/", controllers.CreatePriceChange)
+	api.App.Delete("/api/pricechange/:id", controllers.DeletePriceChange)
 	// Login
 	api.App.Post("/api/login/", controllers.Login)
 }
